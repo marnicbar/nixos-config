0adbe9b45f2f36b7ae15e347e63b8cb6b3015b80
Marius Bär <marius.baer@proton.me>
2024-12-05 19:39:38 +0100

    Update system to NixOS 24.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs.url = "nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     hyprland = {
       url = "github:hyprwm/Hyprland";
