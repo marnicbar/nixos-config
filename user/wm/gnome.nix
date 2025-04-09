c3cc835a7c5a5b5f8b12762e849d6cc02d8660bc
Marius Bär <marius.baer@proton.me>
2025-04-09 15:53:49 +0200

    Set system locale to german

--- user/wm/gnome.nix
+++ user/wm/gnome.nix
@@ -2,11 +2,12 @@
 {
   dconf = {
     enable = true;
+    # Find dconf parameters with: dconf watch /
     settings = {
       "org/gnome/desktop/interface".color-scheme = "prefer-dark";
       "org/gnome/evince/default".continuous = true;
+      "org/gnome/desktop/calendar".show-weekdate = true;
+      "system/locale".region = "de_DE.UTF-8";
     };
-    #org/gnome/evince/default/continuous
-  #true
   };
 }
