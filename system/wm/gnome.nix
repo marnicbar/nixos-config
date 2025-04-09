b3c75f53d40952413417e9734c316fa952288328
Marius Bär <marius.baer@proton.me>
2025-04-09 16:01:32 +0200

    Remove not needed default gnome applications

--- system/wm/gnome.nix
+++ system/wm/gnome.nix
@@ -1,6 +1,20 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  environment.gnome.excludePackages = with pkgs; [
+    epiphany    # Web Browser
+    gedit       # Text Editor
+    simple-scan # Document Scanner
+    yelp        # Help Viewer
+    geary       # Email Client
+    seahorse    # Password Manager
+    gnome-characters
+    gnome-contacts
+    gnome-font-viewer
+    gnome-maps
+    gnome-weather
+    gnome-connections
+  ];
 }
