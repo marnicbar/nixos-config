4ef67ee700aaae380bfd3d473b1cdd4d7a76941c
Marius Bär <marius.baer@proton.me>
2025-04-22 22:15:30 +0200

    Install and configure "auto-power-profile" Gnome extension

--- user/wm/gnome.nix
+++ user/wm/gnome.nix
@@ -1,13 +1,31 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  home.packages = with pkgs.gnomeExtensions; [
+    # Automatically switch between power profiles based on power supply and battery status
+    auto-power-profile
+  ];
   dconf = {
     enable = true;
     # Find dconf parameters with: dconf watch /
     settings = {
-      "org/gnome/desktop/interface".color-scheme = "prefer-dark";
-      "org/gnome/evince/default".continuous = true;
-      "org/gnome/desktop/calendar".show-weekdate = true;
       "system/locale".region = "de_DE.UTF-8";
+      "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+      "org/gnome/desktop/calendar".show-weekdate = true;
+      "org/gnome/evince/default".continuous = true;
+
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = [
+          pkgs.gnomeExtensions.auto-power-profile.extensionUuid
+        ];
+        disabled-extensions = [];
+      };
+      "org/gnome/shell/extensions/auto-power-profile" = {
+        ac = "performance";
+        bat = "balanced";
+        lapmode = true;
+        threshold = 20;
+      };
     };
   };
 }
