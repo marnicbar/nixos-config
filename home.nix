bb4b1735ac2be58e05a1d4fe063ab04d51facec4
Marius Bär <marius.baer@proton.me>
2025-08-05 18:52:31 +0200

    Update system and install protonvpn

--- home.nix
+++ home.nix
@@ -57,6 +57,7 @@
         ]
       ))
       protonmail-desktop
+      protonvpn-gui
       rnote
       rustdesk-flutter # Use rustdesk-flutter over rustdesk because the latter uses unfree components and therefore won't be cached by hydra.
       signal-desktop
