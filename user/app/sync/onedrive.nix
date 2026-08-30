d64488ae697046a7d2592e47036e183a1b57ee50
Marius Bär <me@mariusbaer.de>
2026-08-30 20:46:15 +0200

    Skip symlinks for OneDrive sync, setup ignore dir and disable broken websoket support

--- user/app/sync/onedrive.nix
+++ user/app/sync/onedrive.nix
@@ -5,4 +5,10 @@
     /Dokumente/HKA/
     /Dokumente/Bewerbungen/
   '';
+
+  home.file.".config/onedrive/config".text = ''
+    disable_websocket_support = "true"
+    skip_symlinks = "true"
+    skip_dir = ".direnv|*/.direnv|.venv|*/.venv|node_modules|*/node_modules|__pycache__|*/__pycache__"
+  '';
 }
